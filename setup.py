235d4f5ce284c5b7e2fe87c7af06fdb9b82ae04b
Tyler Pennebaker <tntp@stanford.edu>
2020-05-01 16:19:26 -0700

    Remove unused import os.environ

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # To use a consistent encoding
 from codecs import open
-from os import path, environ
+from os import path
 
 cur_dir = path.abspath(path.dirname(__file__))
 
