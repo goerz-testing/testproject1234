8a4a80e631bb0644d84f6ed8769caf07bb6f64a7
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-23 00:07:06 +0000

    Update docs after building Travis build 104 of hhslepicka/testproject1234
    The docs were built from the tag 'master' against the commit
    4e158b8d871be5ae0ffb5b4d8ac864cb03f599e5.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/hhslepicka/testproject1234/jobs/339122929.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0+8.g386848d',
+    VERSION: '1.0+2.g4e158b8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
